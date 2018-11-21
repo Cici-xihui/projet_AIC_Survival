13ddebb832342a91cbbf0052d54c1a7ecb284428
nnour <nouredine.nour@u-psud.fr>
2018-11-21 21:12:57 +0100

    Merged baseline branch into master

--- sample_code_submission/model.py
+++ sample_code_submission/model.py
@@ -10,17 +10,22 @@ import pickle
 import numpy as np   # We recommend to use numpy arrays
 from os.path import isfile
 from sklearn.base import BaseEstimator
+from sklearn.tree import DecisionTreeRegressor # First baseline method
+
+
 
 class model (BaseEstimator):
     def __init__(self):
         '''
         This constructor is supposed to initialize data members.
-        Use triple quotes for function documentation. 
+        Use triple quotes for function documentation.
         '''
         self.num_train_samples=0
         self.num_feat=1
         self.num_labels=1
         self.is_trained=False
+        # Simple decision tree for the baseline method
+        self.baseline_clf = DecisionTreeRegressor()
 
     def fit(self, X, y):
         '''
@@ -44,6 +49,7 @@ class model (BaseEstimator):
         print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
         if (self.num_train_samples != num_train_samples):
             print("ARRGH: number of samples in X and y do not match!")
+        self.baseline_clf.fit(X, y)
         self.is_trained=True
 
     def predict(self, X):
@@ -64,7 +70,8 @@ class model (BaseEstimator):
         if (self.num_feat != num_feat):
             print("ARRGH: number of features in X does not match training data!")
         print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        y = np.zeros([num_test_samples, self.num_labels])
+        y = self.baseline_clf.predict(X)
+        #y = np.zeros([num_test_samples, self.num_labels])
         # If you uncomment the next line, you get pretty good results for the Iris data :-)
         #y = np.round(X[:,3])
         return y
